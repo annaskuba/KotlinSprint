236fc6120c05447293e87d48b0f550556e9fcdd3
annaskuba <annaskuba222@gmail.com>
2025-10-21 20:16:01 +0300

    KS-1-2 Task 2 - added number of employees and output

---
+++ src/main/kotlin/Lesson1/lesson1_task2.kt
@@ -0,0 +1,18 @@
+package org.example.Lesson1
+
+fun main() {
+
+    var numberOfOrders: Int = 75
+    val thankYouMessage: String = "Спасибо за покупку! :)"
+
+    println(numberOfOrders)
+    println(thankYouMessage)
+
+    var numberOfEmployees = 2000
+
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+
+    println(numberOfEmployees)
+}
