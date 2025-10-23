41b8510a6034ac5028457a6ff5e4d22ae71671bb
annaskuba <annaskuba222@gmail.com>
2025-10-23 22:21:28 +0300

    KS-2-1 change df to String.format

--- src/main/kotlin/Lesson2/lesson2_task1.kt
+++ src/main/kotlin/Lesson2/lesson2_task1.kt
@@ -1,12 +1,6 @@
 package org.example.Lesson2
 
-import java.text.DecimalFormat
-import java.text.DecimalFormatSymbols
-import java.util.Locale
-
 fun main() {
-
-    val df = DecimalFormat("#0.00", DecimalFormatSymbols(Locale.US))
 
     val numberOfStudents = 4
     val grade1 = 3
@@ -14,8 +8,8 @@ fun main() {
     val grade3 = 3
     val grade4 = 5
 
-    val meanGrade = (grade1 + grade2 + grade3 + grade4) / numberOfStudents
+    val meanGrade = (grade1 + grade2 + grade3 + grade4) / numberOfStudents.toDouble()
+    val meanGradeFormatted = String.format("%.2f", meanGrade)
 
-    println(df.format(meanGrade))
-
+    println(meanGradeFormatted)
 }
