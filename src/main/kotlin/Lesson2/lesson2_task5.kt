50b065bdc053b1b7cf38202a368e3d469c83dbc8
annaskuba <annaskuba222@gmail.com>
2025-10-24 11:53:15 +0300

    KS-2-5 changed percent calculation

--- src/main/kotlin/Lesson2/lesson2_task5.kt
+++ src/main/kotlin/Lesson2/lesson2_task5.kt
@@ -3,14 +3,17 @@ package org.example.Lesson2
 import kotlin.math.pow
 import java.util.Locale
 
-const val INTEREST_RATE = 0.167
+const val INTEREST_RATE_PERCENTAGE = 16.7
+const val PERCENT_FORMATTER = 100
+
 
 fun main() {
 
+    val interestRate: Double = INTEREST_RATE_PERCENTAGE / PERCENT_FORMATTER
     val initialSum = 70000
     val years = 20
 
-    val totalSum = initialSum * (1 + INTEREST_RATE).pow(years)
+    val totalSum = initialSum * (1 + interestRate).pow(years)
     println(String.format(Locale.US, "%.3f", totalSum))
 
 }
